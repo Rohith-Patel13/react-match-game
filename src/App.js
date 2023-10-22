0a5ba9b1c0022e68108aba9fe1dada21e6ce72b7
Rohith Patel <rohithgithub999@gmail.com>
2023-10-22 06:24:00 +0000

    match game 1st commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// app.js
+
+import HomePageComponent from './Components/HomePageComponent/index'
+
+import './App.css'
+
+// Replace your code here
+const App = () => <HomePageComponent />
+
+export default App
